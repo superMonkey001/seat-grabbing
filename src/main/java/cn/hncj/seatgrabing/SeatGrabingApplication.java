e32eb9a4ec5e23d51e1bca2a1b0c1c7dd7ae8757
superMonkey001 <3102405586@qq.com>
2022-10-04 17:34:04 +0800

    first commit

---
+++ src/main/java/cn/hncj/seatgrabing/SeatGrabingApplication.java
@@ -0,0 +1,13 @@
+package cn.hncj.seatgrabing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatGrabingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeatGrabingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/hncj/seatgrabing/SeatGrabingApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hncj.seatgrabing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatGrabingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
